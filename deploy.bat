108ee0e8c53f7efb14f19a710962fd7ca01c06c4
bestproduct <dellewisruntle27741@gmail.com>
2026-05-12 21:26:36 +0800

    auto update

--- deploy.bat
+++ deploy.bat
@@ -1,22 +1,13 @@
 @echo off
-echo ==========================================
-echo      Starting Auto-Deployment to GitHub
-echo ==========================================
 
-:: 1. 添加所有更改
-echo [1/3] Adding files...
+echo Adding files...
 git add .
 
-:: 2. 提交更改 (自动生成包含时间的备注)
-echo [2/3] Committing changes...
-set "timestamp=%date% %time%"
-git commit -m "Auto-update: %timestamp%"
+echo Committing...
+git commit -m "auto update"
 
-:: 3. 推送到 GitHub
-echo [3/3] Pushing to GitHub...
+echo Pushing...
 git push
 
-echo ==========================================
-echo      Success! Website updated.
-echo ==========================================
+echo Done!
 pause
